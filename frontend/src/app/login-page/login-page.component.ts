cae2aff16b8462186e0762865c1aed03556662ac
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2024-02-22 09:17:16 +0100

    [ADD] login page

---
+++ frontend/src/app/login-page/login-page.component.ts
@@ -0,0 +1,72 @@
+import {Component, OnInit} from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
+import {FormsModule, NgForm} from "@angular/forms";
+import { take } from 'rxjs';
+// import { User } from '../models/user.model';
+// import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [
+    FormsModule,
+    RouterLink
+  ],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent{
+  submit = false;
+  isLoading !: boolean;
+  // userConnected!: User;
+  errorLogin !: any | undefined;
+
+
+  // constructor(private router: Router,
+  //             protected authService: AuthService) {}
+
+  ngOnInit() {
+    this.isLoading = false;
+  }
+
+
+  onSubmit(f: NgForm) {
+
+    this.errorLogin = undefined;
+    this.submit = true;
+    this.isLoading = true;
+
+
+    // if (f.value.email != ""  && f.value.password != "" && !this.errorLoginExist()){
+    //   this.authService.login(f.value.email, f.value.password)
+    //       .subscribe
+    //       (user => {
+    //             this.userConnected = user;
+    //             this.router.navigateByUrl("/home");
+    //           },
+    //           error => {
+    //             console.error('Erreur lors de la connexion :', error.error.message);
+    //             this.errorLogin = error.error;
+    //           })
+    //
+    // }
+
+  }
+
+  userConnectedIsLoaded() {
+    // return this.userConnected !== undefined;
+  }
+
+  errorLoginExist() {
+    return this.errorLogin !== undefined;
+  }
+
+  errorIsEmail() {
+    return this.errorLogin.type === 'email';
+  }
+
+  errorIsPassword() {
+    return this.errorLogin.type === 'password';
+  }
+
+}
