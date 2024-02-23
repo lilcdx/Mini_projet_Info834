31d2d0e6d87791654ced5e0290865ed0d98f069c
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2024-02-23 18:37:31 +0100

    [ADD] home page into app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginPageComponent} from "./login-page/login-page.component";
 import {RegisterPageComponent} from "./register-page/register-page.component";
+import {HomePageComponent} from "./home-page/home-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginPageComponent, RegisterPageComponent],
+  imports: [RouterOutlet, LoginPageComponent, RegisterPageComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
