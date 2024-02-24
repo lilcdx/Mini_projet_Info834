231e00c6f90e452e85951331c3ab9a232cc1062f
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2024-02-24 15:56:07 +0100

    [ADD] sidebar in app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {LoginPageComponent} from "./login-page/login-page.component";
 import {RegisterPageComponent} from "./register-page/register-page.component";
 import {HomePageComponent} from "./home-page/home-page.component";
+import {SidebarComponent} from "./sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginPageComponent, RegisterPageComponent, HomePageComponent],
+    imports: [RouterOutlet, LoginPageComponent, RegisterPageComponent, HomePageComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
