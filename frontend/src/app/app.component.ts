fdc3986c0df6ce2942ee05f3eb81e3fa47bd77c5
lilcdx <lila.cadoux@gmail.com>
2024-03-04 14:16:11 +0100

    [ADD] User component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginPageComponent} from "./login-page/login-page.component";
 import {RegisterPageComponent} from "./register-page/register-page.component";
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginPageComponent, RegisterPageComponent],
+  imports: [RouterOutlet, LoginPageComponent, RegisterPageComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
