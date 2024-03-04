48fc6c2d69cce638d084122bd94e526dd56ede8b
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2024-03-04 16:23:52 +0100

    [MODIF] style user

--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-user',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-
+  @Input() user!: User;
 }
