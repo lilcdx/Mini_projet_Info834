97bf9576941824b54ba3fce54c86a51e2737d730
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2024-03-06 22:26:11 +0100

    [AMELIORATION] conversation page code

--- frontend/src/app/conversation-page/conversation-page.component.ts
+++ frontend/src/app/conversation-page/conversation-page.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit, Input, ViewChild, ElementRef} from '@angular/core';
-import { ActivatedRoute, Router, RouterLink, RoutesRecognized } from "@angular/router";
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink } from "@angular/router";
 import { SocketioService } from '../services/socketio.service';
 import { User } from '../models/user.model';
 import { ChatService } from '../services/chat.service';
@@ -11,201 +11,177 @@ import { MessageSentComponent } from "../message-sent/message-sent.component";
 import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-conversation-page',
-  standalone: true,
-  imports: [
-    RouterLink,
-    FormsModule,
-    MessageSentComponent
-  ],
-  templateUrl: './conversation-page.component.html',
-  styleUrl: './conversation-page.component.scss'
+    selector: 'app-conversation-page',
+    standalone: true,
+    imports: [
+        RouterLink,
+        FormsModule,
+        MessageSentComponent
+    ],
+    templateUrl: './conversation-page.component.html',
+    styleUrl: './conversation-page.component.scss'
 })
 export class ConversationPageComponent implements OnInit {
 
-  @Input() userConnected!: User;
-  @ViewChild('content') private myScrollContainer!: ElementRef;
+    @Input() userConnected!: User;
+    @ViewChild('content') private myScrollContainer!: ElementRef;
 
-  APPLICATION_GLOBAL_ROOM = "myRandomChatRoomId";
+    APPLICATION_GLOBAL_ROOM = "myRandomChatRoomId";
 
-  userSelected!: User;
-  chatId!: number;
-  listMessages: Message[] = [];
+    userSelected!: User;
+    chatId!: number;
+    listMessages: Message[] = [];
 
-  usersIds: Number[] = [];
+    usersIds: Number[] = [];
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private socketService: SocketioService,
-    private chatService: ChatService,
-    private messageService: MessageService,
-    private userService: UserService
-  ) { }
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private socketService: SocketioService,
+        private chatService: ChatService,
+        private messageService: MessageService,
+        private userService: UserService
+    ) { }
 
-  ngOnInit() {
-    console.log("------------------------------ CONVERSATION PAGE");
+    ngOnInit() {
+        console.log("------------------------------ CONVERSATION PAGE");
 
+        // Appeler l'initialisation une première fois lors de l'initialisation du composant
+        this.initializeData();
 
-    const userSelectedId = this.route.snapshot.params['id'];
-
-
-    // -- BDD
-
-    // Get user selected
-    this.userService.getUserById(userSelectedId)
-      .subscribe((data: any) => {
-        this.userSelected = data;
-
-        this.socketService.getUsers(this.APPLICATION_GLOBAL_ROOM).subscribe(users => {
-          this.usersIds = Object.values(users).map((userData: any) => {
-            return userData.id;
-          });
-
-          // check if this.userSelected.id is in this.usersIds
-          if (this.usersIds.includes(this.userSelected.id)) {
-            console.log("USER ONLINE");
-            this.userSelected.online = true;
-          } else {
-            console.log("USER OFFLINE");
-            this.userSelected.online = false;
-          }
+        // Surveiller les événements de navigation pour réagir aux changements d'URL
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                console.log('URL a changé : ', event.url);
+                // this.initializeData();
+            }
         });
-
-      });
-
-    // Get chat between 2 users
-    this.chatService.getChatBetweenUsers(this.userConnected.id, userSelectedId)
-      .subscribe((data: any) => {
-
-        this.chatId = data.id;
-
-        // If no chat between the 2 users exist -> Create a new Chat
-        if (data.length == 0) {
-
-          // Create chat in BDD
-          this.chatService.createChat(this.userConnected.id, userSelectedId)
-            .subscribe((data: any) => {
-              console.log("NEW CHAT CREATE ");
-              console.log(data);
-
-              // -- SOCKET
-              // Join the room named with the chat ID get in BDD
-              this.socketService.joinConnectionChat(data.chat._id, this.userConnected, userSelectedId, (err: any, data: any[]) => {
-                // console.log(data);
-              });
-
-              // Subscribe to the messages to be inform when a new message is send
-              this.socketService.subscribeToMessages((err: any, data: any) => {
-                // console.log(data);
-
-                // Add the new message to the list of messages
-                this.listMessages.push(
-                  new Message(
-                    data.id,
-                    data.idChat,
-                    data.content,
-                    data.timestamp,
-                    data.idSender
-                  )
-                );
-
-              });
-
-              // -- BDD
-              // Get all messages from the chat              
-              this.messageService.getMessagesByChatId(data.chat._id)
-                .subscribe((data: any) => {
-                  console.log("MESSAGES GET : ");
-                  console.log(data);
-
-                  this.listMessages = data;
-
-                });
-
-            });
-        } else {
-          console.log("CHAT GET ");
-          console.log(data);
-
-
-          // -- SOCKET
-          // Join the room named with the chat ID get in BDD
-          this.socketService.joinConnectionChat(data.id, this.userConnected, userSelectedId, (err: any, data: any[]) => {
-            // console.log(data);
-          });
-
-          // Subscribe to the messages to be inform when a new message is send
-          this.socketService.subscribeToMessages((err: any, data: any) => {
-            // console.log(data);
-
-            // Add the new message to the list of messages
-            this.listMessages.push(
-              new Message(
-                data.id,
-                data.idChat,
-                data.content,
-                data.timestamp,
-                data.idSender
-              )
-            );
-          });
-
-          // -- BDD
-          // Get all messages from the chat
-          this.messageService.getMessagesByChatId(data.id)
-            .subscribe((data: any) => {
-              console.log("MESSAGES GET : ");
-              console.log(data);
-
-              this.listMessages = data;
-
-            });
-
-        }
-
-      });
-
-    // Create chat
-    // this.chatService.createChat(this.userConnected.id, userSelectedId)
-    //   .subscribe((data: any) => {
-    //     console.log(data);
-
-    //     // 
-    //   });
-  }
-
-  onSubmit(f: NgForm) {
-
-    if (f.value.content != "") {
-
-
-      // -- BDD
-      // Create a message in BDD
-      this.messageService.createMessage(this.chatId, f.value.content, this.userConnected.id)
-        .subscribe((data: any) => {
-          // console.log(data);
-        });
-
-
-      // -- SOCKET
-      // Send the message to the socket
-      this.socketService.sendMessage(f.value.content, (data: any) => {
-        // console.log(data);
-      });
-
-        // // -- vider le champ
-        f.resetForm();
-
-
     }
 
-  }
+    initializeData() {
+        const userSelectedId = this.route.snapshot.params['id'];
 
-  adjustHeight(event: any) {
-    event.target.style.height = 'inherit';
-    event.target.style.height = `${event.target.scrollHeight}px`;
-  }
+        // Réinitialiser la liste de messages
+        this.listMessages = [];
+
+        // Charger l'utilisateur sélectionné
+        this.userService.getUserById(userSelectedId)
+            .subscribe((data: any) => {
+                this.userSelected = data;
+
+                // Vérifier si l'utilisateur sélectionné est en ligne
+                this.socketService.getUsers(this.APPLICATION_GLOBAL_ROOM).subscribe(users => {
+                    this.usersIds = Object.values(users).map((userData: any) => {
+                        return userData.id;
+                    });
+
+                    if (this.usersIds.includes(this.userSelected.id)) {
+                        console.log("USER ONLINE");
+                        this.userSelected.online = true;
+                    } else {
+                        console.log("USER OFFLINE");
+                        this.userSelected.online = false;
+                    }
+                });
+            });
+
+        // Récupérer le chat entre les 2 utilisateurs
+        this.chatService.getChatBetweenUsers(this.userConnected.id, userSelectedId)
+            .subscribe((data: any) => {
+                this.chatId = data.id;
+
+                // Si aucun chat entre les 2 utilisateurs n'existe -> Créer un nouveau chat
+                if (data.length == 0) {
+                    this.chatService.createChat(this.userConnected.id, userSelectedId)
+                        .subscribe((data: any) => {
+                            console.log("NEW CHAT CREATE ");
+                            console.log(data);
+
+                            // Join the room named with the chat ID get in BDD
+                            this.socketService.joinConnectionChat(data.chat._id, this.userConnected, userSelectedId, (err: any, data: any[]) => {
+                                // console.log(data);
+                            });
+
+                            // Subscribe to the messages to be inform when a new message is send
+                            this.socketService.subscribeToMessages((err: any, data: any) => {
+                                // console.log(data);
+                                this.listMessages.push(
+                                    new Message(
+                                        data.id,
+                                        data.idChat,
+                                        data.content,
+                                        data.timestamp,
+                                        data.idSender
+                                    )
+                                );
+                            });
+
+                            // Get all messages from the chat
+                            this.messageService.getMessagesByChatId(data.chat._id)
+                                .subscribe((data: any) => {
+                                    console.log("MESSAGES GET : ");
+                                    console.log(data);
+                                    // Assurez-vous de vider la liste de messages avant d'ajouter de nouveaux messages
+                                    this.listMessages = data;
+                                });
+                        });
+                } else {
+                    console.log("CHAT GET ");
+                    console.log(data);
+
+                    // Join the room named with the chat ID get in BDD
+                    this.socketService.joinConnectionChat(data.id, this.userConnected, userSelectedId, (err: any, data: any[]) => {
+                        // console.log(data);
+                    });
+
+                    // Subscribe to the messages to be inform when a new message is send
+                    this.socketService.subscribeToMessages((err: any, data: any) => {
+                        // console.log(data);
+                        this.listMessages.push(
+                            new Message(
+                                data.id,
+                                data.idChat,
+                                data.content,
+                                data.timestamp,
+                                data.idSender
+                            )
+                        );
+                    });
+
+                    // Get all messages from the chat
+                    this.messageService.getMessagesByChatId(data.id)
+                        .subscribe((data: any) => {
+                            console.log("MESSAGES GET : ");
+                            console.log(data);
+                            // Assurez-vous de vider la liste de messages avant d'ajouter de nouveaux messages
+                            this.listMessages = data;
+                        });
+                }
+            });
+    }
+
+
+    onSubmit(f: NgForm) {
+        if (f.value.content != "") {
+            // Create a message in BDD
+            this.messageService.createMessage(this.chatId, f.value.content, this.userConnected.id)
+                .subscribe((data: any) => {
+                    // console.log(data);
+                });
+
+            // Send the message to the socket
+            this.socketService.sendMessage(f.value.content, (data: any) => {
+                // console.log(data);
+            });
+
+            f.resetForm();
+        }
+    }
+
+    adjustHeight(event: any) {
+        event.target.style.height = 'inherit';
+        event.target.style.height = `${event.target.scrollHeight}px`;
+    }
 
     ngAfterViewChecked() {
         this.scrollToBottom();
@@ -219,6 +195,6 @@ export class ConversationPageComponent implements OnInit {
         ) - window.innerHeight;
         try {
             window.scrollTo(0, valeurMinimaleScroll);
-        } catch(err) { }
+        } catch (err) { }
     }
 }
